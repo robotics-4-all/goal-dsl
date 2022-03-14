10007fef756f3ed3ab5de4920d6c0186f92b7ecf
Konstantinos Panayiotou <klpanagi@gmail.com>
2022-03-14 02:14:08 +0200

    Work on new generator

--- goal_dsl/__init__.py
+++ goal_dsl/__init__.py
@@ -1,9 +1,16 @@
-import os
+"""
+Top level package directory of goal-dsl
+"""
+
+
+__author__ = """Konstantinos Panayiotou"""
+__email__ = 'klpanagi@gmail.com'
+__version__ = "0.2.0.dev"
+
+
 from textx import language
 
 from goal_dsl.utils import get_mm
-
-__version__ = "0.2.0.dev"
 
 
 @language('goal_dsl', '*.goal')
