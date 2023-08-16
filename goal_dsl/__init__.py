1219f24c0ead8847f682bc0b5139deb8415cbaa6
Konstantinos Panayiotou <klpanagi@gmail.com>
2023-08-17 02:57:55 +0300

    Several improvements

--- goal_dsl/__init__.py
+++ goal_dsl/__init__.py
@@ -5,15 +5,6 @@ Top level package directory of goal-dsl
 
 __author__ = """Konstantinos Panayiotou"""
 __email__ = 'klpanagi@gmail.com'
-__version__ = "0.2.0.dev"
+__version__ = "0.1.0"
 
-
-from textx import language
-
-from goal_dsl.utils import get_mm
-
-
-@language('goal_dsl', '*.goal')
-def goal_dsl_language():
-    "goal_dsl language"
-    return get_mm()
+from .language import goaldsl_language, get_metamodel
